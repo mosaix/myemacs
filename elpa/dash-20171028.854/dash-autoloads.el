359b76431ce8d9b996242cf2bc0a4ad4decb502d
ThomasSun <thomassun@irigi.net>
2017-11-16 18:24:21 +0800

    1day complete.

---
+++ elpa/dash-20171028.854/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("dash.el") (23053 22761 237330 752000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
